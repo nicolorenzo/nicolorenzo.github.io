569e76bff56bed7773656bfac0b61d74696f2d0c
nicolorenzo <nicolorenzo@live.com>
2022-07-08 19:33:16 -0300

    Adicionando função que oculta o menu

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ $('.slider').slick({
     },
   ],
 });
+$(document).ready(function () {
+  'use strict';
+
+  var c,
+    currentScrollTop = 0,
+    navbar = $('nav');
+
+  $(window).scroll(function () {
+    var a = $(window).scrollTop();
+    var b = navbar.height();
+
+    currentScrollTop = a;
+
+    if (c < currentScrollTop && a > b + b) {
+      navbar.addClass('scrollUp');
+    } else if (c > currentScrollTop && !(a <= b)) {
+      navbar.removeClass('scrollUp');
+    }
+    c = currentScrollTop;
+  });
+});
 
 function loadBarsOnScroll() {
   let progressBars = document.querySelectorAll('.progress > div');
