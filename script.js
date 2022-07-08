22eab100c61a3c9ba4dec38bebefbccfe0a527a0
nicolorenzo <nicolorenzo@live.com>
2022-07-08 19:15:46 -0300

    Adicionando função que carrega a barra de skill

--- script.js
+++ script.js
@@ -16,24 +16,24 @@ $('.slider').slick({
     },
   ],
 });
-$(document).ready(function () {
-  'use strict';
 
-  var c,
-    currentScrollTop = 0,
-    navbar = $('nav');
+function loadBarsOnScroll() {
+  let progressBars = document.querySelectorAll('.progress > div');
 
-  $(window).scroll(function () {
-    var a = $(window).scrollTop();
-    var b = navbar.height();
-
-    currentScrollTop = a;
-
-    if (c < currentScrollTop && a > b + b) {
-      navbar.addClass('scrollUp');
-    } else if (c > currentScrollTop && !(a <= b)) {
-      navbar.removeClass('scrollUp');
+  for (let progressBar of progressBars) {
+    // console.log(+progressBar.dataset.porcentagem);
+    var scrollOffset = $(document).scrollTop();
+    var containerOffset = progressBar.offsetTop - window.innerHeight;
+    if (scrollOffset > containerOffset) {
+      progressBar.style.width = progressBar.dataset.porcentagem + '%';
+      // unbind event not to load scrolsl again
+      // $(document).unbind('scroll');
     }
-    c = currentScrollTop;
-  });
+  }
+}
+
+loadBarsOnScroll();
+document.addEventListener('scroll', (event) => {
+  // console.log('aaaa');
+  loadBarsOnScroll();
 });
